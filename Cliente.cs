9810917872bce269e3052bb39ac60740aaf9ac5d
Pierre <040797pierre@gmail.com>
2022-05-16 10:21:07 -0300

    update Program && Cliente

--- Cliente.cs
+++ Cliente.cs
@@ -4,11 +4,67 @@ namespace MarcenariaMarcelus
 {
     public class Cliente
     {
-        string nome;
-        string CPF_CNPJ;
-        DateTime data_nascimento;
-        string telefone;
-        string email;
-        string endereco;
+        private string nome;
+        private string CPF_CNPJ;
+        private DateTime data_nascimento;
+        private string telefone;
+        private string email;
+        private string endereco;
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return(nome);
+        }
+        
+        public void setCPF_CNPJ(string CPF_CNPJ)
+        {
+            this.CPF_CNPJ = CPF_CNPJ;
+        }
+        public string getCPF_CNPJ()
+        {
+            return(CPF_CNPJ);
+        }
+        
+        public void setDataNascimento(DateTime data_nascimento)
+        {
+            this.data_nascimento = data_nascimento;
+        }
+        public DateTime getDataNascimento()
+        {
+            return (data_nascimento);
+        }
+        public void setTelefone(string telefone)
+        {
+            this.telefone = telefone;
+        }
+
+        public string getTelefone()
+        {
+            return (telefone);
+        }
+        
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public string getEmail()
+        {
+            return (email);
+        }
+        
+        public void setEndereco(string endereco)
+        {
+            this.endereco = endereco;
+        }
+
+        public string getEndereco()
+        {
+            return (endereco);
+        }
     }
 }
